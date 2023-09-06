a63e88b5a3ffe34202ca2ddfecdd01c4fbd371c2
Kollishion <126138338+Kollishion@users.noreply.github.com>
2023-09-06 22:54:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// var istatus = document.querySelector("h5");
-
-// var btn = document.querySelector("#add");
-// var check = 0;
-
-// btn.addEventListener("click", function(){
-//     if(check == 0){
-//         istatus.innerHTML = "Friends";
-//         istatus.style.color = "green";
-//         btn.innerHTML = "Remove Friend"
-//         check = 1;
-//     }else{
-//         istatus.innerHTML = "Stranger";
-//         istatus.style.color = "red";
-//         btn.innerHTML = "Add Friend";
-//         check = 0;
-//     }
-// })
 var con = document.querySelector("#container");
 var love = document.querySelector("i");
 
