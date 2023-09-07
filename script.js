176a2ab6cd939e62be1045405cb5735b577d3857
Kollishion <126138338+Kollishion@users.noreply.github.com>
2023-09-07 14:35:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-//Card Accept/Remove Friend Request
-// var istatus = document.querySelector("h5");
-
-// var btn = document.querySelector("#add");
-// var check = 0;
-
-// btn.addEventListener("click", function(){
-//     if(check == 0){
-//         istatus.innerHTML = "Friends";
-//         istatus.style.color = "green";
-//         btn.innerHTML = "Remove Friend"
-//         check = 1;
-//     }else{
-//         istatus.innerHTML = "Stranger";
-//         istatus.style.color = "red";
-//         btn.innerHTML = "Add Friend";
-//         check = 0;
-//     }
-// })
-//Instagram double click like
-// var con = document.querySelector("#container");
-// var love = document.querySelector("i");
-
-// con.addEventListener("dblclick", function(){
-//     love.style.transform = "translate(-50%, -50%) scale(1)";
-//     love.style.opacity = "0.8";
-//     setTimeout(function(){
-//         love.style.opacity = "0";
-//     }, 1000)
-//     setTimeout(function(){
-//         love.style.transform = "translate(-50%, -50%) scale(0)";
-//     }, 2000)
-// })
 var main = document.querySelector("#main");
 var crsr = document.querySelector(".cursor");
 
